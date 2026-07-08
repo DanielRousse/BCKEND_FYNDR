5c63df86137ffeb862cd59793d4df381ef7cb805
Jonathan Reyes <reyesgordillojonathandaniel@gmail.com>
2026-07-08 14:01:56 -0600

    Se inicializa github y se sube la estructura del backend

---
+++ src/main/java/org/generation/fyndr/FyndrApplication.java
@@ -0,0 +1,13 @@
+package org.generation.fyndr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyndrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FyndrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/fyndr/FyndrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.fyndr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyndrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
